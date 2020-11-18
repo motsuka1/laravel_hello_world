ab0dd686bfa8f4ec965fd1650889ee7b7d0ce158
Masaki Otsuka <motsuka1996@gmail.com>
2020-11-18 15:21:29 +0900

    initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>Welcome!</h1>
+    <h2>Access to <a href="http://127.0.0.1:8000/hello"> the hello page</a></h2>
+  </body>
+</html>
